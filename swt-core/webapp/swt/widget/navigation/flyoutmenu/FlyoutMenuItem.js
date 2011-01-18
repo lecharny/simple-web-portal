b04fd76fb2ab4e6fbb301c56994590a5dc305969
hans.harjeet <hans.harjeet@050c1aa1-6f06-c1c4-0079-e7737e84d742>
2011-01-18 16:50:15 +0000

    added flyout inner menu

--- swt-core/webapp/swt/widget/navigation/flyoutmenu/FlyoutMenuItem.js
+++ swt-core/webapp/swt/widget/navigation/flyoutmenu/FlyoutMenuItem.js
@@ -2,15 +2,15 @@ dojo.provide("swt.widget.navigation.flyoutmenu.FlyoutMenuItem");
 dojo.require("dijit._Widget");
 dojo.require("dijit._Templated");
 dojo.require("dijit._Contained");
-
-dojo.declare("swt.widget.navigation.flyoutmenu.FlyoutMenuItem",[dijit._Widget, dijit._Templated, dijit._Contained],{
+dojo.require("dijit._Container");
+dojo.declare("swt.widget.navigation.flyoutmenu.FlyoutMenuItem",[dijit._Widget, dijit._Templated, dijit._Contained, dijit._Container],{
 
 	baseClass: "flyoutMenuItem",
     
     templateString: dojo.cache("swt", "widget/navigation/flyoutmenu/templates/FlyoutMenuItem.html"),
     
     attributeMap: dojo.delegate(dijit._Widget.prototype.attributeMap, {
-		label: { node: "containerNode", type: "innerHTML" }
+		label: { node: "labelNode", type: "innerHTML" }
 	}),
 	
 	// iconClass for the item, default size is 16x16, override if needed.
@@ -26,14 +26,18 @@ dojo.declare("swt.widget.navigation.flyoutmenu.FlyoutMenuItem",[dijit._Widget, d
     
     label: "",
     
+    hasInnerMenu: false,
+    
+    innerMenuReference: null,
+    
+    innerMenuIndicator: null,
+    
     constructor : function() {
 		this.inherited(arguments);
-        console.log("constructor");
     },
 
     postMixInProperties : function() {
     	this.inherited(arguments);
-        console.log("postMixInProperties");
     },
 
     postCreate : function() {
@@ -41,26 +45,38 @@ dojo.declare("swt.widget.navigation.flyoutmenu.FlyoutMenuItem",[dijit._Widget, d
     	if(this.iconClass && this.iconClass.length>1){
     		dojo.addClass(this.containerNode, this.iconClass);
     	}
-    	console.log("postCreate");
     },
 
     startup : function() {
+   	 	if (this._started) {
+   	 		return;
+   	 	}
+   	 	var _im = dojo.query("ul",this.containerNode);
+    	if(_im.length>0){
+    		this.hasInnerMenu = true;
+    		this.innerMenuReference = _im;
+    		this.connectInnerMenu();
+    		console.log("Inner menu found");
+    	}
     	this.inherited(arguments);
-        console.log("startup");
+
     },
     
     destroy: function(){
     	this.inherited(arguments);
-    	console.log("destroy");
     },
 
     _onMouseOver: function(evt){
     	dojo.addClass(this.domNode, this.hoverClass);
+    	this.getParent()._onItemOver(this, evt);
     	this.onMouseOver(evt);
+    	dojo.stopEvent(evt);
     },
     _onMouseOut: function(evt){
     	dojo.removeClass(this.domNode, this.hoverClass);
+    	this.getParent()._onItemOut(this, evt);
     	this.onMouseOut(evt);
+    	dojo.stopEvent(evt);
     },
     _onClick: function(evt){
     	console.log("_onClick called!");
@@ -81,5 +97,25 @@ dojo.declare("swt.widget.navigation.flyoutmenu.FlyoutMenuItem",[dijit._Widget, d
     onMouseOut: function(evt){
     	// summary:
     	//		Call-back to connect to on mouse out. 
+    },
+
+    connectInnerMenu: function(){
+
+    	this.innerMenuIndicator = dojo.create("span", {'class':'subMenuIndicator', innerHTML:'&nbsp;&nbsp;'}, this.labelNode, "last"); 
+    		    
+    		
+    		/*
+    		ultags[t].parentNode.onmouseover=function(){
+    		    this.getElementsByTagName("ul")[0].style.left=this.parentNode.offsetWidth+submenuoffset+"px"
+    		    this.getElementsByTagName("ul")[0].style.display="block"
+    		    }
+    		    ultags[t].parentNode.onmouseout=function(){
+    		    this.getElementsByTagName("ul")[0].style.display="none"
+    		    }
+    		    
+    		    
+    		    }
+    		  }*/
     }
+
 });
