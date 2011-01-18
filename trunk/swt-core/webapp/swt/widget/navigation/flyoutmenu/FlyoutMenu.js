4e0b4ae8c445a5a2c95c87d541850cee0f8ceeef
hans.harjeet <hans.harjeet@050c1aa1-6f06-c1c4-0079-e7737e84d742>
2011-01-18 16:50:15 +0000

    added flyout inner menu

--- trunk/swt-core/webapp/swt/widget/navigation/flyoutmenu/FlyoutMenu.js
+++ trunk/swt-core/webapp/swt/widget/navigation/flyoutmenu/FlyoutMenu.js
@@ -2,8 +2,8 @@ dojo.provide("swt.widget.navigation.flyoutmenu.FlyoutMenu");
 dojo.require("dijit._Widget");
 dojo.require("dijit._Templated");
 dojo.require("dijit._Container");
-
-dojo.declare("swt.widget.navigation.flyoutmenu.FlyoutMenu",[dijit._Widget, dijit._Templated, dijit._Container],{
+dojo.require("dijit._Contained");
+dojo.declare("swt.widget.navigation.flyoutmenu.FlyoutMenu",[dijit._Widget, dijit._Templated, dijit._Container, dijit._Contained],{
 
 	baseClass: "flyoutMenu",
     
@@ -45,6 +45,10 @@ dojo.declare("swt.widget.navigation.flyoutmenu.FlyoutMenu",[dijit._Widget, dijit
     	this.selected = item;
     },
     
+    _onItemOver: function(item, evt){
+    },
+    _onItemOut: function(item, evt){
+    },
     getSelected: function(){
     	return this.selected;
     },
