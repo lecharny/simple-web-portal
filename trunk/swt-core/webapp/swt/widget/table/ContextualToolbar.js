d6176e6d9da142099264b04fa66766edaa0dbfd8
hans.harjeet <hans.harjeet@050c1aa1-6f06-c1c4-0079-e7737e84d742>
2011-08-16 00:59:44 +0000

    added some eventing and row selection

--- trunk/swt-core/webapp/swt/widget/table/ContextualToolbar.js
+++ trunk/swt-core/webapp/swt/widget/table/ContextualToolbar.js
@@ -7,6 +7,18 @@ dojo.declare('swt.widget.table.ContextualToolbar', [dijit.Toolbar], {
 	baseClass:"swtTableContextualToolbar",
 	templateString: dojo.cache("swt", "widget/table/templates/ContextualToolbar.html"),
 	widgetsInTemplate: true,
+	/////////////////////
+	// NLS Labels START//
+	/////////////////////
+	labelEdit: "Edit",
+	labelDelete: "Delete",
+	labelAddRow:"Add Row",
+	labelClearSelection:"Clear Selections",
+	/////////////////////
+	// NLS Labels END////
+	/////////////////////
+	// reference to table this toolbar is for.
+	table: null,
 	
 	buildRendering: function(){
 		this.inherited(arguments);
@@ -18,6 +30,19 @@ dojo.declare('swt.widget.table.ContextualToolbar', [dijit.Toolbar], {
 	
 	postCreate: function(){
 		this.inherited(arguments);
+	},
+	
+	edit: function(evt){
+		this.table.edit();
+	},
+	deleteRow: function(evt){
+		this.table.deleteRow();
+	},
+	addRow: function(evt){
+		this.table.addRow();
+	},
+	clearSelection: function(evt){
+		this.table.clearSelection();
 	}
 	
 });
