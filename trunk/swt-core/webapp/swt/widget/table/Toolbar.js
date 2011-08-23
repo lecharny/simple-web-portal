a7208bafcc518f0738b1d5dcc5637efd1ac0ca5f
hans.harjeet <hans.harjeet@050c1aa1-6f06-c1c4-0079-e7737e84d742>
2011-08-23 00:58:20 +0000

    added column show-hide menu.

--- trunk/swt-core/webapp/swt/widget/table/Toolbar.js
+++ trunk/swt-core/webapp/swt/widget/table/Toolbar.js
@@ -3,6 +3,8 @@ dojo.provide("swt.widget.table.Toolbar");
 dojo.require("dijit.Toolbar");
 dojo.require("dijit.layout.ContentPane");
 dojo.require("dijit.form.DropDownButton");
+dojo.require("dijit.Menu");
+dojo.require("dijit.CheckedMenuItem");
 
 dojo.declare('swt.widget.table.Toolbar', [dijit.Toolbar], {
 
@@ -36,10 +38,37 @@ dojo.declare('swt.widget.table.Toolbar', [dijit.Toolbar], {
 	postCreate: function(){
 		this.inherited(arguments);
 		//this.connect(this.refreshAP, "onClick", dojo.hitch(this, "refreshTable"));
+		this._render();
 	},
 	
 	refreshTable: function(evt){
 		this.table.refresh(evt);
+	},
+	_render: function(){
+		var _menu = new dijit.Menu({label:"Structure Menu", showLabel:false});
+		var structure = this.table.structure;
+		dojo.forEach(structure.columns, function(column, idx, arr){
+			if(column.isSelector || column.isRowCounter){
+				// no need to hide these.
+			} else {
+				_menu.addChild(new dijit.CheckedMenuItem({label:column.label}));
+			}
+		});
+		_menu.startup();
+		dojo.place(_menu.domNode, this.settingsContentAP.domNode, "first");
+		this.connect(_menu, "onClick", dojo.hitch(this, "_onMenuClick"));
+	},
+	_onMenuClick: function(evt){
+		console.log("clicked on menu.");
+	},
+	_ok: function(evt){
+		console.log("clicked on ok.");
+		
+	},
+	_cancel: function(evt){
+		console.log("clicked on cancel.");
+		
 	}
+
 	
 });
