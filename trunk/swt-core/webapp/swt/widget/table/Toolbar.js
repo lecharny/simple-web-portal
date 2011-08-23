798e5493d0cc0000eb5756a8b114e637880424b1
hans.harjeet <hans.harjeet@050c1aa1-6f06-c1c4-0079-e7737e84d742>
2011-08-23 23:37:19 +0000

    added column show/hide.

--- trunk/swt-core/webapp/swt/widget/table/Toolbar.js
+++ trunk/swt-core/webapp/swt/widget/table/Toolbar.js
@@ -2,6 +2,7 @@ dojo.provide("swt.widget.table.Toolbar");
 
 dojo.require("dijit.Toolbar");
 dojo.require("dijit.layout.ContentPane");
+dojo.require("dijit.form.Button");
 dojo.require("dijit.form.DropDownButton");
 dojo.require("dijit.Menu");
 dojo.require("dijit.CheckedMenuItem");
@@ -19,10 +20,12 @@ dojo.declare('swt.widget.table.Toolbar', [dijit.Toolbar], {
 	/////////////////////
 	refresh : "refresh",
 	settings :"settings",
+	close:"Close",
 	/////////////////////
 	// NLS Labels END////
 	/////////////////////
 
+	_menu: null,
 	
 	buildRendering: function(){
 		this.inherited(arguments);
@@ -33,6 +36,7 @@ dojo.declare('swt.widget.table.Toolbar', [dijit.Toolbar], {
 		var _m = this.table.messages;
 		this.refresh = (_m[this.refresh])?_m[this.refresh]:"Refresh";
 		this.settings = (_m[this.settings])?_m[this.settings]:"Settings";
+		this.close = (_m[this.close])?_m[this.close]:"Close";
 	},
 	
 	postCreate: function(){
@@ -45,30 +49,49 @@ dojo.declare('swt.widget.table.Toolbar', [dijit.Toolbar], {
 		this.table.refresh(evt);
 	},
 	_render: function(){
-		var _menu = new dijit.Menu({label:"Structure Menu", showLabel:false});
+		var _self = this;
+		this._menu = new dijit.Menu({label:"Structure Menu", showLabel:false});
 		var structure = this.table.structure;
 		dojo.forEach(structure.columns, function(column, idx, arr){
 			if(column.isSelector || column.isRowCounter){
 				// no need to hide these.
 			} else {
-				_menu.addChild(new dijit.CheckedMenuItem({label:column.label}));
+				var _args = {label:column.label, onClick: dojo.hitch(_self, "_onMenuClick"), _columnIndex: idx};
+				if(column.hidden){
+					_args.checked = false;
+				} else {
+					_args.checked = true;
+				}
+				_self._menu.addChild(new dijit.CheckedMenuItem(_args));
 			}
 		});
-		_menu.startup();
-		dojo.place(_menu.domNode, this.settingsContentAP.domNode, "first");
-		this.connect(_menu, "onClick", dojo.hitch(this, "_onMenuClick"));
+		//_menu.startup();
+		dojo.place(this._menu.domNode, this.settingsContentAP.domNode, "first");
+		//this.connect(this._menu, "onClick", dojo.hitch(this, "_onMenuClick"));
+		
+		var _okButton = new dijit.form.Button({label:this.close, onClick: dojo.hitch(this, "_close")});
+		dojo.place(_okButton.domNode, this.actionsAP, "last");
+		
+		
 	},
 	_onMenuClick: function(evt){
-		console.log("clicked on menu.");
+		var _mi = dijit.byNode(evt.currentTarget);
+		if(_mi._columnIndex){
+			if(_mi.get("checked")){
+				this.table.showColumn(this._menu.getIndexOfChild(_mi));
+			} else {
+				this.table.hideColumn(this._menu.getIndexOfChild(_mi));
+			}
+			console.log("Clicked on menu item-->" + this.table.structure.columns[_mi._columnIndex].label + " INDEX " + this._menu.getIndexOfChild(_mi));
+		}
 	},
 	_ok: function(evt){
 		console.log("clicked on ok.");
 		
 	},
-	_cancel: function(evt){
-		console.log("clicked on cancel.");
+	_close: function(evt){
+		console.log("clicked on close.");
 		
 	}
-
 	
 });
