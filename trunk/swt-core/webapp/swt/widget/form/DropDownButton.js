e9cebed7e6e978a3d485595eac425172ea2e3bc6
hans.harjeet <hans.harjeet@050c1aa1-6f06-c1c4-0079-e7737e84d742>
2011-09-22 01:05:41 +0000

    added dropdown button

--- trunk/swt-core/webapp/swt/widget/form/DropDownButton.js
+++ trunk/swt-core/webapp/swt/widget/form/DropDownButton.js
@@ -1,50 +1,77 @@
 dojo.provide("swt.widget.form.DropDownButton");
-dojo.require("dijit._Widget");
-dojo.require("dijit._Templated");
 
-dojo.declare("swt.widget.form.DropDownButton",[dijit._Widget, dijit._Templated],{
+dojo.require("swt.widget.form._DropDownMixin");
 
-	baseClass: "swtbtn",
-    
-    templateString: dojo.cache("swt", "widget/form/templates/DropDownButton.html"),
-    
-    iconClass:"",
-    
-    label:"",
-    
-    href:"#",
-    
-    showLabel: true,
+dojo.declare("swt.widget.form.DropDownButton",[swt.widget.form.Button, swt.widget.form._DropDownMixin],{
+
+	additionalClass: "swtbtndropdown",
+	
+	dropDown: null,
+	
+    dropDownOffset: -2,
     
     postCreate : function() {
-    	if(this.iconClass.length>1){
-    		var _html = "<span class='btnicon "+ this.iconClass +"'></span>"; 
-    		dojo.place(_html, this.domNode, "first");
-    	}
+    	dojo.addClass(this.domNode, this.additionalClass);
     	this.inherited(arguments);
     },
     startup : function() {
-    	 if (this._started) {
-             return;
-         }
-         var _self = this;
-         dojo.forEach(this.getChildren(), function(child){
-         	if(!this._started){
-         		child.startup();
-         	}
-         });
+    	if (this._started) {
+    		return;
+    	}
+   	 	var _im = dojo.query(".dropDown", this.domNode);
+   	 	if(_im.length>0){
+   	 		this.dropDown = _im[0];
+        	this.connectDropDown();
+        	console.log("DropDown found");
+        }
+       	// set up watchers
+       	this.watch("dropDown", dojo.hitch(this, "setDropDown"));
          
     	this.inherited(arguments);
     },
-    destroy: function(){
-    	this.inherited(arguments);
-    },
+    
     onClick: function(evt){
-    	dojo.style(this.ddNode, "display","block");
     	// callback for onClick.
     	if(this.href.length>1){
     		evt.preventDefault();
     	}
+    },
+
+    connectDropDown: function(){
+    	// no support for inner menu if using as horizontal.
+    	// comment it out if want to support horizontal.
+    	//if(this.getParent().getAlignment()=="horizontal"){
+    	//	return;
+    	//}
+    	//this.innerMenuIndicator = dojo.create("span", {'class':'subMenuIndicator dijitArrowButtonInner', innerHTML:''}, this.labelNode, "last");
+
+    	this.connect(this.dropDown, "onclick", function(evt){
+    		dojo.stopEvent(evt);
+    	});
     	
-    }
+    	if(this.openDropDownOn=="hover"){
+        	this.connect(this.domNode, "onmouseover", dojo.hitch(this, "_show"));
+        	this.connect(this.domNode, "onmouseout", dojo.hitch(this, "_hide"));
+    	} else {
+        	this.connect(this, "onClick", dojo.hitch(this, "toggle"));
+    	}
+    	
+    },
+
+    setDropDown: function(/*object*/ aDropDown){
+    	// summary: sets the dropdown if not set already.
+    	// aDropDown: a widget or a domNode
+    	if(aDropDown){
+        	if(aDropDown.declaredClass){
+        		if(!aDropDown._started){
+        			aDropDown.startup();
+        		}
+        		this.dropDown = aDropDown.domNode;
+        	} else {
+            	this.dropDown = aDropDown;
+        	}
+        	dojo.place(this.dropDown, this.domNode);
+        	this.connectDropDown();
+    	}
+    }    
 });
---
+++ trunk/swt-core/webapp/swt/widget/form/_DropDownMixin.js
@@ -0,0 +1,40 @@
+dojo.provide("swt.widget.form._DropDownMixin");
+
+dojo.declare("swt.widget.form._DropDownMixin",[],{
+
+	toggle: function(evt){
+		// summary: toggles the open/close state.
+    	if(this.dropDown.isOpen){
+    		this._hide(evt);
+    	} else {
+    		this._show(evt);
+    	}
+    },
+    _show: function(evt){
+    	// summary: show the drop down
+    	var _l="", _t="";
+    	if(this.getParent && this.getParent().getAlignment()=="vertical"){
+    		_l = this.domNode.offsetWidth + this.dropDownOffset + "px";
+    		dojo.style(this.dropDown, {'visibility':'visible','left': _l});
+    	} else {
+        	var _mb = dojo.position(this.domNode, true);
+        	//console.log(dojo.toJson(_mb));
+    		_l = 0 + "px";
+    		_t = _mb.h+ this.dropDownOffset + "px";
+    		//console.log("_l, _t::" + _l +" - "+ _t);
+    		dojo.style(this.dropDown, {'visibility':'visible','left': _l, 'top': _t});
+
+    	}
+    	dojo.addClass(this.domNode,"dropDownOpen");
+    	
+    	this.dropDown.isOpen = true;
+    },
+    
+    _hide: function(evt){
+    	// summary: hide the drop down    	
+    	dojo.style(this.dropDown, {'visibility':'hidden'});
+    	dojo.removeClass(this.domNode,"dropDownOpen");
+    	this.dropDown.isOpen = false;
+    }
+
+});
