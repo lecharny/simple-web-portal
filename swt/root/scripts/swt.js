aa287a994090ae062f7ac91b0bbd424a81eca2a8
hans.harjeet <hans.harjeet@050c1aa1-6f06-c1c4-0079-e7737e84d742>
2011-10-07 23:56:02 +0000

    added swt project

---
+++ swt/root/scripts/swt.js
@@ -0,0 +1,12 @@
+require(["twitter-bootstrap/js/bootstrap-twipsy"]);
+require(["twitter-bootstrap/js/bootstrap-alerts"]);
+require(["twitter-bootstrap/js/bootstrap-dropdown"]);
+require(["twitter-bootstrap/js/bootstrap-modal"]);
+require(["twitter-bootstrap/js/bootstrap-popover"]);
+require(["twitter-bootstrap/js/bootstrap-scrollspy"]);
+require(["twitter-bootstrap/js/bootstrap-tabs"]);
+
+require(["swt/util/common"], function() {
+    console.log("All swt is loaded now.");
+});
+
